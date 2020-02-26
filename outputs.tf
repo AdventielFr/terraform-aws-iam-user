87289d1e3d6bf1ca72480336b303683df56771b0
Gwendall Garnier <gwendall@MacBook-Pro-de-Gwendall.local>
2020-02-26 16:45:19 +0100

    fix remove access_key creation because is not idempotent

--- outputs.tf
+++ outputs.tf
@@ -16,20 +16,10 @@ output "user_name" {
 
 output "user_encrypted_password" {
   description = "The encrypted password, base64 encoded."
-  value       = join("", aws_iam_user_login_profile.this.*.encrypted_password)
+  value       = var.console_access ? aws_iam_user_login_profile.this[0].encrypted_password : ""
 }
 
 output "user_key_fingerprint" {
   description = "The fingerprint of the PGP key used to encrypt the password."
-  value       = join("", aws_iam_user_login_profile.this.*.key_fingerprint)
-}
-
-output "user_access_key" {
-  description = "The access key ID."
-  value       = join("", aws_iam_access_key.this.*.id)
-}
-
-output "user_secret_key" {
-  description = "The secret access key. Note that this will be written to the state file."
-  value       = join("", aws_iam_access_key.this.*.secret)
+  value       = var.console_access ? aws_iam_user_login_profile.this[0].key_fingerprint : ""
 }
